d6cb158df6376a3ca729c0cc02f1fba224e4682b
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-04-20 15:48:28 +0530

    Create invoices.sql

---
+++ invoices.sql
@@ -0,0 +1,10 @@
+CREATE TABLE invoices (
+    InvoiceNo VARCHAR(256),
+    StockCode      VARCHAR(256),
+    Description VARCHAR(256),
+    Quantity       INT,
+    InvoiceDate    DATETIME,
+    UnitPrice      float, 
+    Customer VARCHAR(256),
+    Country  VARCHAR(256),
+)
